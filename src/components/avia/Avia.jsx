c0d5b363a8250548989a7a5b2f3fc50a3dd34f9a
Alanondoto <alexgusevtoo@gmail.com>
2022-11-14 18:42:33 +0300

    pre-final-version of Avia component

--- src/components/avia/Avia.jsx
+++ src/components/avia/Avia.jsx
@@ -1,25 +1,92 @@
+import React, { useState } from 'react';
 import './avia.css'
 import calendar_1 from '../../assets/calendar_1.svg';
 import calendar_2 from '../../assets/calendar_2.svg';
+import {useNavigate} from 'react-router-dom'
 
-export const Avia = () => {
+export const Avia = (props) => {
+
+  const fromDateInput = React.createRef();
+
+  const focusFromDateInput = () => {
+    fromDateInput.current.showPicker();
+  }
+
+  const toDateInput = React.createRef();
+
+  const focusToDateInput = () => {
+    toDateInput.current.showPicker();
+  }
+
+  const navigate = useNavigate();
+
+  const navigateToInfo = () => {
+    navigate('/avia/info');
+  }
+
+  const [navigateFrom, setNavigateFrom] = useState('');
+
+  const changeNavigateFrom = (e) => {
+    setNavigateFrom(e.currentTarget.value);
+    console.log(e.currentTarget.value)
+  }
+
+  const [navigateTo, setNavigateTo] = useState('');
+
+  const changeNavigateTo = (e) => {
+    setNavigateTo(e.currentTarget.value);
+    console.log(e.currentTarget.value)
+  }
+
+  const [flyThere, setFlyThere] = useState('');
+
+  const changeFlyThere = (e) => {
+    setFlyThere(e.currentTarget.value);
+    console.log(e.currentTarget.value)
+  }
+
+  const [flyBack, setFlyBack] = useState('');
+
+  const changeFlyBack = (e) => {
+    setFlyBack(e.currentTarget.value);
+    console.log(e.currentTarget.value)
+  }
+
   return (
-    <div className="searchItemsWrapper">
-        <div className='searchItem'>
-            <input type="text" placeholder='Город вылета' className='distInput' />
+    <>
+        <div className="searchItemsWrapper">
+            <div className='searchItem'>
+                <div className='distInputHeader'>Откуда</div>
+                <div className='distInputWrapper'>
+                    <input value={navigateFrom} onChange={changeNavigateFrom} type="text" placeholder='Город вылета' className='distInput' />
+                </div>
+            </div>
+            <div className='searchItem'>
+                <div className='distInputHeader'>Куда</div>
+                <div className="distInputWrapper">
+                    <input value={navigateTo} onChange={changeNavigateTo} type="text" placeholder='Город прилета' className='distInput' />
+                </div>
+            </div>
+            <div className='searchItem'>
+                <div className='distInputHeader'>Туда</div>
+                <div className="distInputWrapper">
+                    <img onClick={focusFromDateInput} src={calendar_1} alt="calendar_1" className='calendar' />
+                    <input value={flyThere} onChange={changeFlyThere} ref={fromDateInput} type="date" placeholder='дд.мм.гг' className='distInput' />
+                </div>
+            </div>
+            <div className="divider"></div>
+            <div className='searchItem'>
+                <div className='distInputHeader'>Обратно</div>
+                <div className="distInputWrapper">
+                    <img onClick={focusToDateInput} src={calendar_2} alt="calendar_2" className='calendar' />
+                    <input value={flyBack} onChange={changeFlyBack} ref={toDateInput} type="date" placeholder='дд.мм.гг' className='distInput' />
+                </div>
+            </div>
         </div>
-        <div className='searchItem'>
-            <input type="text" placeholder='Город прилета' className='distInput' />
+        <div className="findTicketsWrapper">
+            <button onClick={navigateToInfo} className='findTicketsBtn'>Найти билеты</button>
         </div>
-        <div className='searchItem'>
-            <img src={calendar_1} alt="calendar_1" />
-            <input type="text" placeholder='дд.мм.гг' className='distInput' />
-        </div>
-        <div className='searchItem'>
-            <img src={calendar_2} alt="calendar_2" />
-            <input type="text" placeholder='дд.мм.гг' className='distInput' />
-        </div>
-    </div>
+    </>
   )
 }
 
